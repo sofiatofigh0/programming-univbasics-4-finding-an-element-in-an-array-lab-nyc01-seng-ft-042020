e91e375a854c740bd7eb205da348bc972e0b6dc8
sofia tofigh <sst2140@columbia.edu>
2020-04-03 00:39:06 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   counter=0
   while counter < array.length do 
     if array[counter] == value_to_find
-      puts counter
+      return counter
     end
    counter=counter + 1
   end
